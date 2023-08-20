4bfa4a9436b4f7c0bd9bf12737ac7b5bea793f72
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-08-20 19:20:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import MovieDetails from './pages/MovieDetails';
@@ -8,13 +8,25 @@ import Reviews from './pages/Reviews';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<MovieDetails />} />
-      <Route path="/movies/:movieId/cast" element={<Cast />} />
-      <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-    </Routes>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      </Routes>
+    </div>
   );
 };
 
